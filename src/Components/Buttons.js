d8674bfab6b46deebe15418a7e052a331912ed05
Ruby <jainruby89@gmail.com>
2022-07-11 05:03:00 +0530

    3rd commit

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './Buttons.css';
 
 export default class Buttons extends Component {
-  label = 'Class Based Component';
-  render() {
-    const handlealert = () => {
-      alert("You have clicked on class based component");
+    label = 'Class Based Component';
+    handlealert = () => {
+        alert("You have clicked on class based component");
     }
-    return <div className='container-fluid'>
-      <button id='btn' onClick={handlealert} className='btn btn-primary'>{this.label}</button>
-    </div>;
-  }
+    render() {
+        return <div className='container-fluid'>
+            <button id='btn' onClick={this.handlealert} className='btn btn-primary'>{this.label}</button>
+        </div>;
+
+    }
+
 }
-export {Buttons};
+export { Buttons };
